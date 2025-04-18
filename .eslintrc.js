ed115dcddacf6669e5298d0f1e9d0b6ad28b5125
SonPham <phamcongson297@gmail.com>
2025-04-18 15:53:29 +0700

    Thêm .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@next/next/no-img-element': 'off',
+    },
+  };
+  
